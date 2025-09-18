1deb91a09bbc12936394bdd0792b94df0e2ec848
StevenValdez <156959937+StevenValdez@users.noreply.github.com>
2025-09-18 02:33:17 +0000

    Finished assignments 2 and 3

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,6 +1,13 @@
 fn assignment1(){
     //Temp converter
 }
+
+fn assignment2(){
+    //Number Analyzer
+}
+fn assignment3(){
+    //Guessing Game
+}
 //Tempature Converter
 const FREEZINGPOINT : f32  = 32.0;
 
@@ -15,7 +22,28 @@ fn celsius_to_fehrenheit(c : f32) -> f32{
     return fehrenheit
 }
 
+// Number Analyzer
+fn is_even(n: i32) -> bool{
+    if n%2 == 0 {
+        return true
+    }
+    else{
+        return false
+    };
+}
+//Guesing Game
 
+fn check_guess(guess : i32, secret : i32) -> i32 {
+    if guess == secret {
+        return 0
+    }
+    else if guess < secret{
+        return -1
+    }
+    else {
+        return 1
+    }
+}
 
 fn main(){
      assignment1();
@@ -36,7 +64,68 @@ fn main(){
         y= y+1.0;
         i+=1;
     }
+    assignment2();
+    println!("Assingment 2 ");
 
-   
+    let array = [100,20,3,4,5,60,7,8,9,10];
+
+    let mut i = 0;
+
+    while i<array.len(){
+        println!("{}",is_even(array[i]));
+        if array[i]%3 == 0{
+            println!("Fizz");
+        }
+        else if array[i]%5 == 0{
+            println!("Buzz");
+        }
+        else if array[i]%3 == 0 && array[i]%5 == 0{
+            println!("FizzBuzz");
+        }
+        i+=1;
+    }
+
+    let mut j = 0;
+    let mut total = 0;
+
+    while j < array.len(){
+        total = array[j]+total;
+       j+=1;
+    };
+   println!("{}",total);
+
+   let mut k = 0;
+   let mut largestNum = array[0];
+    while k < array.len(){
+       if array[k]>largestNum{
+        largestNum = array[k];
+       }
+        k+=1;
+
+    }
+    println!("{}",largestNum);
+
+
+    assignment3();
+    let mut myNum:i32  = 56;
+    let guesses = [24,86,13,5,56];
+    let mut guess_count = 0;
+    for guess in guesses {
+        let result = check_guess(guess,myNum);
+        if result == 1 {
+            println!{"Oops {} is too high!",guess};
+        }
+        else if result == -1{
+            println!{"Uh-Oh {} is too low",guess};
+        }
+        else{
+            println!{"{} Is right you got it!",guess};
+            
+        }
+        guess_count+=1;
+    }
+    println!{"It took you {} Guesses",guess_count};
     
+
+
 }
